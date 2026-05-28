1d794c19e6ad7dcad7a346314c9e86dbd6912f90
Sin <deathlysin@outlook.com>
2026-05-28 22:00:41 +0100

    Revert Song Preview plugin back to my repo so any updates get grabbed again at build time now that I'm out of account purgatory

--- scripts/build-common.sh
+++ scripts/build-common.sh
@@ -137,7 +137,6 @@ clone_slopsmith() {
 		byrongamatos/slopsmith-plugin-profileimport
 		byrongamatos/slopsmith-plugin-sectionmap
 		byrongamatos/slopsmith-plugin-setlist
-		byrongamatos/slopsmith-plugin-song-preview
 		byrongamatos/slopsmith-plugin-stepmode
 		byrongamatos/slopsmith-plugin-studio
 		byrongamatos/slopsmith-plugin-tabimport
@@ -147,6 +146,7 @@ clone_slopsmith() {
 		# Community plugins
 		alleexx/slopsmith-plugin-transpose-chords
 		ChrisBeWithYou/slopsmith-plugin-slopscale
+    DeathlySin/slopsmith-plugin-song-preview
 		Jafz2001/slopsmith-plugin-nam-rig-builder
 		masc0t/slopsmith-plugin-find-more
 		masc0t/slopsmith-plugin-invert-highway
